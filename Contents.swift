dd65c2b923d09c198f66558c418b0c6fef7cb37a
Cheng <chengtan2013@gmail.com>
2020-04-14 10:46:45 -0400

    AES spec

---
+++ Contents.swift
@@ -0,0 +1,213 @@
+import Foundation
+import CommonCrypto
+
+protocol Cryptable {
+    func encrypt(_ string: String) throws -> Data
+    func decrypt(_ data: Data) throws -> String
+}
+
+struct AES {
+    private let key: Data
+    private let ivSize: Int         = kCCBlockSizeAES128
+    private let options: CCOptions  = CCOptions(kCCOptionPKCS7Padding)
+
+    init(key: Data) throws {
+        guard key.count == kCCKeySizeAES256 else {
+            throw Error.invalidKeySize
+        }
+        self.key = key
+    }
+}
+
+extension AES {
+    enum Error: Swift.Error {
+        case invalidKeySize
+        case generateRandomIVFailed
+        case encryptionFailed
+        case decryptionFailed
+        case dataToStringFailed
+        case keyGeneration(status: Int)
+            
+    }
+}
+
+private extension AES {
+
+    func generateRandomIV(for data: inout Data) throws {
+
+        try data.withUnsafeMutableBytes { dataBytes in
+
+            guard let dataBytesBaseAddress = dataBytes.baseAddress else {
+                throw Error.generateRandomIVFailed
+            }
+
+            let status: Int32 = SecRandomCopyBytes(
+                kSecRandomDefault,
+                kCCBlockSizeAES128,
+                dataBytesBaseAddress
+            )
+
+            guard status == 0 else {
+                throw Error.generateRandomIVFailed
+            }
+        }
+    }
+}
+
+extension AES: Cryptable {
+    func encrypt(_ string: String) throws -> Data {
+        let dataToEncrypt = Data(string.utf8)
+
+        let bufferSize: Int = ivSize + dataToEncrypt.count + kCCBlockSizeAES128
+        var buffer = Data(count: bufferSize)
+        try generateRandomIV(for: &buffer)
+
+        var numberBytesEncrypted: Int = 0
+
+        do {
+            try key.withUnsafeBytes { keyBytes in
+                try dataToEncrypt.withUnsafeBytes { dataToEncryptBytes in
+                    try buffer.withUnsafeMutableBytes { bufferBytes in
+
+                        guard let keyBytesBaseAddress = keyBytes.baseAddress,
+                            let dataToEncryptBytesBaseAddress = dataToEncryptBytes.baseAddress,
+                            let bufferBytesBaseAddress = bufferBytes.baseAddress else {
+                                throw Error.encryptionFailed
+                        }
+
+                        let cryptStatus: CCCryptorStatus = CCCrypt( // Stateless, one-shot encrypt operation
+                            CCOperation(kCCEncrypt),                // op: CCOperation
+                            CCAlgorithm(kCCAlgorithmAES),           // alg: CCAlgorithm
+                            options,                                // options: CCOptions
+                            keyBytesBaseAddress,                    // key: the "password"
+                            key.count,                              // keyLength: the "password" size
+                            bufferBytesBaseAddress,                 // iv: Initialization Vector
+                            dataToEncryptBytesBaseAddress,          // dataIn: Data to encrypt bytes
+                            dataToEncryptBytes.count,               // dataInLength: Data to encrypt size
+                            bufferBytesBaseAddress + ivSize,        // dataOut: encrypted Data buffer
+                            bufferSize,                             // dataOutAvailable: encrypted Data buffer size
+                            &numberBytesEncrypted                   // dataOutMoved: the number of bytes written
+                        )
+
+                        guard cryptStatus == CCCryptorStatus(kCCSuccess) else {
+                            throw Error.encryptionFailed
+                        }
+                    }
+                }
+            }
+
+        } catch {
+            throw Error.encryptionFailed
+        }
+
+        let encryptedData: Data = buffer[..<(numberBytesEncrypted + ivSize)]
+        return encryptedData
+    }
+
+    func decrypt(_ data: Data) throws -> String {
+        let bufferSize: Int = data.count - ivSize
+        var buffer = Data(count: bufferSize)
+
+        var numberBytesDecrypted: Int = 0
+
+        do {
+            try key.withUnsafeBytes { keyBytes in
+                try data.withUnsafeBytes { dataToDecryptBytes in
+                    try buffer.withUnsafeMutableBytes { bufferBytes in
+
+                        guard let keyBytesBaseAddress = keyBytes.baseAddress,
+                            let dataToDecryptBytesBaseAddress = dataToDecryptBytes.baseAddress,
+                            let bufferBytesBaseAddress = bufferBytes.baseAddress else {
+                                throw Error.encryptionFailed
+                        }
+
+                        let cryptStatus: CCCryptorStatus = CCCrypt( // Stateless, one-shot encrypt operation
+                            CCOperation(kCCDecrypt),                // op: CCOperation
+                            CCAlgorithm(kCCAlgorithmAES128),        // alg: CCAlgorithm
+                            options,                                // options: CCOptions
+                            keyBytesBaseAddress,                    // key: the "password"
+                            key.count,                              // keyLength: the "password" size
+                            dataToDecryptBytesBaseAddress,          // iv: Initialization Vector
+                            dataToDecryptBytesBaseAddress + ivSize, // dataIn: Data to decrypt bytes
+                            bufferSize,                             // dataInLength: Data to decrypt size
+                            bufferBytesBaseAddress,                 // dataOut: decrypted Data buffer
+                            bufferSize,                             // dataOutAvailable: decrypted Data buffer size
+                            &numberBytesDecrypted                   // dataOutMoved: the number of bytes written
+                        )
+
+                        guard cryptStatus == CCCryptorStatus(kCCSuccess) else {
+                            throw Error.decryptionFailed
+                        }
+                    }
+                }
+            }
+        } catch {
+            throw Error.encryptionFailed
+        }
+
+        let decryptedData: Data = buffer[..<numberBytesDecrypted]
+
+        guard let decryptedString = String(data: decryptedData, encoding: .utf8) else {
+            throw Error.dataToStringFailed
+        }
+
+        return decryptedString
+    }
+    
+    static func randomData(length: Int) -> Data {
+        var data = Data(count: length)
+        let status = data.withUnsafeMutableBytes { mutableBytes in
+            SecRandomCopyBytes(kSecRandomDefault, length, mutableBytes)
+        }
+        assert(status == Int32(0))
+        return data
+    }
+    
+    static func randomSalt() -> Data {
+        return randomData(length: 8)
+    }
+    
+    static func createKey(password: Data, salt: Data) throws -> Data {
+        let length = kCCKeySizeAES256
+        var status = Int32(0)
+        var derivedBytes = [UInt8](repeating: 0, count: length)
+        password.withUnsafeBytes { (passwordBytes: UnsafePointer<Int8>!) in
+            salt.withUnsafeBytes { (saltBytes: UnsafePointer<UInt8>!) in
+                status = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2),                  // algorithm
+                                              passwordBytes,                                // password
+                                              password.count,                               // passwordLen
+                                              saltBytes,                                    // salt
+                                              salt.count,                                   // saltLen
+                                              CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA1),   // prf
+                                              10000,                                        // rounds
+                                              &derivedBytes,                                // derivedKey
+                                              length)                                       // derivedKeyLen
+            }
+        }
+        guard status == 0 else {
+            throw Error.keyGeneration(status: Int(status))
+        }
+        return Data(bytes: UnsafePointer<UInt8>(derivedBytes), count: length)
+    }
+}
+
+do {
+    let password = "foo"
+    let salt = AES.randomSalt()
+    let key = try AES.createKey(password: password.data(using: .utf8)!, salt: salt)
+    print("key:\t\t\t\(key.count)")
+    
+    let aes = try AES(key: key)
+
+    let stringToEncrypt: String = "-40.12345678"
+    print("String to encrypt:\t\t\t\(stringToEncrypt)")
+
+    let encryptedData: Data = try aes.encrypt(stringToEncrypt)
+    print("String encrypted (base64):\t\(encryptedData.base64EncodedString())")
+
+    let decryptedData: String = try aes.decrypt(encryptedData)
+    print("String decrypted:\t\t\t\(decryptedData)")
+
+} catch {
+    print("Something went wrong: \(error)")
+}
